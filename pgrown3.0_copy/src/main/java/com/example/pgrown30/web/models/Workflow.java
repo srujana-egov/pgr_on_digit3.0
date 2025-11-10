e6396cf1356f67c13ff0f859f349ca715dc2f1e5
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 14:31:08 +0530

    Update Workflow.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/Workflow.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/Workflow.java
@@ -1,37 +1,29 @@
 package com.example.pgrown30.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import jakarta.validation.Valid;
-
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Workflow {
 
     @JsonProperty("action")
-    @Size(min = 1, max = 64, message = "Action should not be more than 64 characters or less than 2 characters.")
+    @Size(min = 1, max = 64)
     private String action;
 
     @JsonProperty("assignes")
-    private java.util.List<String> assignes;            
+    private List<String> assignes;
 
     @JsonProperty("comments")
-    @Size(min = 1, max = 64, message = "Comments should not be more than 64 characters or less than 2 characters.")
+    @Size(min = 1, max = 64)
     private String comments;
 
     @JsonProperty("verificationDocuments")
     @Valid
-    private java.util.List<Document> verificationDocuments;        
-
-    
+    private List<Document> verificationDocuments;
 }
-
