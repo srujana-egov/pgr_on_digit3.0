bb7218323129b729b55861da98fc50ca3dce2ec1
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 14:31:58 +0530

    Update ServiceWrapper.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/ServiceWrapper.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/ServiceWrapper.java
@@ -1,32 +1,22 @@
 package com.example.pgrown30.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class ServiceWrapper {
 
     @JsonProperty("CitizenService")
-    @NotNull(message = "Service cannot be blank")
+    @NotNull
     @Valid
     private CitizenService service;
 
     @JsonProperty("Workflow")
     @Valid
     private Workflow workflow;
-
-
-    
 }
-
-
