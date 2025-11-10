766160d2af7db6a1ce34bb1713181b42030b33d8
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 14:32:18 +0530

    Update ServiceResponse.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/ServiceResponse.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/ServiceResponse.java
@@ -2,11 +2,7 @@ package com.example.pgrown30.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import java.util.List;
 
 @Data
