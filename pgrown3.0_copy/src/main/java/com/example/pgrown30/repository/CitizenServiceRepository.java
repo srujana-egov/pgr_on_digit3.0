8d62c2abe0431efa0ae48ae73f509fe0dfe4d670
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 15:17:19 +0530

    Update CitizenServiceRepository.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/repository/CitizenServiceRepository.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/repository/CitizenServiceRepository.java
@@ -1,16 +1,17 @@
 package com.example.pgrown30.repository;
-
-import com.example.pgrown30.domain.CitizenServiceEntity;
-import com.example.pgrown30.domain.Status;
+import com.example.pgrown30.web.models.CitizenService;
+import com.example.pgrown30.web.models.Status;// <--- new import
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CitizenServiceRepository extends JpaRepository<CitizenServiceEntity, String>, JpaSpecificationExecutor<CitizenServiceEntity> {
+
+public interface CitizenServiceRepository extends JpaRepository<CitizenService, String>, JpaSpecificationExecutor<CitizenService> {
     List<CitizenServiceEntity> findByTenantId(String tenantId);
 
     List<CitizenServiceEntity> findByTenantIdAndServiceCode(String tenantId, String serviceCode);
