1fe3d2f0e73cbe833e90e3f1d1bb52eb83406df1
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 14:31:38 +0530

    Update Document.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/Document.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/Document.java
@@ -1,10 +1,7 @@
 package com.example.pgrown30.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
