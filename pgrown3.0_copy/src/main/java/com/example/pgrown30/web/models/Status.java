a2a50288a99ea0a3757225c3e0491a78c7e0b53f
srujana-egov <srujana.dadi@egovernments.org>
2025-11-10 14:29:39 +0530

    Create Status.java

---
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/Status.java
@@ -0,0 +1,12 @@
+package com.example.pgrown30.web.models;
+
+public enum Status {
+    INITIATED,
+    IN_PROGRESS,
+    COMPLETED,
+    REJECTED,
+    VERIFIED,
+    APPROVED,
+    ACTIVE,
+    INACTIVE
+}
