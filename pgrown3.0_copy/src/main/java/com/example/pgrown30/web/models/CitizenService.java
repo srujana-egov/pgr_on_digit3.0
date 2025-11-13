9d3999c18835b19e8cdc57771e808a9fd43449cb
srujana-egov <srujana.dadi@egovernments.org>
2025-11-13 05:32:09 +0530

    Update CitizenService.java

--- pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/CitizenService.java
+++ pgrown3.0_copy/src/main/java/com/example/pgrown30/web/models/CitizenService.java
@@ -1,29 +1,37 @@
-package com.example.pgrown30.web.models;
+// CitizenService.java
+package com.example.pgr.client.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import java.io.Serializable;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CitizenService {
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CitizenService implements Serializable {
     private String serviceRequestId;
     private String tenantId;
     private String serviceCode;
-    private String description;
-    private String accountId;
-    private String source;
-    private String applicationStatus; // String representation of Status enum
-    private String fileStoreId;
-    private boolean fileValid;
-    private String boundaryCode;
-    private boolean boundaryValid;
-    private String action;
-    private String workflowInstanceId;
-    private Long createdTime;
-    private Long lastModifiedTime;
-    private String email;       // <-- add this
-    private String mobile; 
+    @JsonProperty("description")
+    private String descriptionText;
+    private AddressDetail addressDetail;
+    private String firstName;
+    private String phone;
+    private String email;
+    private Status status;
 
+    public enum Status { OPEN, ASSIGNED, RESOLVED, REJECTED, CLOSED }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class AddressDetail {
+        private String addressLine1;
+        private String city;
+        private String pincode;
+    }
 }
